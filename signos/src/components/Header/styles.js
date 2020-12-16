5f1ba3ab60ca0e9ddcefbf21ee048e1f1b4e4d01
Matheus <matheus.novaes998@gmail.com>
2020-12-16 14:33:12 -0300

    Header pronto

---
+++ signos/src/components/Header/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+export const HeaderStyled = styled.header`
+background-color: #F29F05;
+height: 100px;
+padding: 1px;
+padding-left: 40px;
+box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+`
