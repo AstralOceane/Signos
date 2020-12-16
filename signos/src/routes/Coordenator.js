c667071948a0a138bd82207bfc33e5072e344b83
Diegodossantos23 <diegodossantos900@outlook.com>
2020-12-16 13:25:58 -0300

    Criei as rotas para cada pagina

--- signos/src/routes/Coordenator.js
+++ signos/src/routes/Coordenator.js
@@ -10,16 +10,16 @@ export const goToAquariosPage = () => {
   history.push("/aquarios")
 }
 
-export const goToAquariosPage = () => {
-  history.push("/aquarios")
-}
-
 export const goToAriesPage = () => {
   history.push("/aries")
 }
 
 export const goToCancerPage = () => {
   history.push("/cancer")
+}
+
+export const goToCancerPage = () => {
+  history.push("/capricornio")
 }
 
 export const goToEscorpiaoPage = () => {
@@ -29,3 +29,29 @@ export const goToEscorpiaoPage = () => {
 export const goToGemeosPage = () => {
   history.push("/gemeos")
 }
+
+export const goToLeaoPage = () => {
+  history.push("/leao")
+}
+
+export const goToLibraPage = () => {
+  history.push("/libra")
+}
+
+export const goToPeixesPage = () => {
+  history.push("/peixes")
+}
+
+export const goToSargitarioPage = () => {
+  history.push("/sargitario")
+}
+
+export const goToTouroPage = () => {
+  history.push("/touro")
+}
+
+export const goToVirgemPage = () => {
+  history.push("/virgem")
+}
+
+
